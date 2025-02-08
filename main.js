932f17f477b0589f0a05915b295ac1d8a3e41d33
AsBorn007 <pk175040@gmail.com>
2025-02-09 04:02:42 +0530

    use (this) for current context

---
+++ main.js
@@ -0,0 +1,10 @@
+const user = {
+    username : "Praveen",
+    age:26,
+   
+    letShowMessege: function (){
+        console.log(`${this.username}, we welcome you `)
+
+    }
+}
+user.letShowMessege()
