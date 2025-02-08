4922f8aa5338a7773714778e1213559774f46666
AsBorn007 <pk175040@gmail.com>
2025-02-09 04:13:22 +0530

    learn what happend when we  change the context

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const user = {
    
     letShowMessege: function (){
         console.log(`${this.username}, we welcome you `)
-
+        console.log(this)  // this give use context of this fuction 
     }
+   
 }
-user.letShowMessege()
+user.letShowMessege();   // right now it retun the current context are in the user  and user name Praveen 
+
+user.username = "ASborn"; // this time we change the  current context of this username form  user object 
+
+user.letShowMessege();// right now it retun the current context are in the user  and user name Asborn 
